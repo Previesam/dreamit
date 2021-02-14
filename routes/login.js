4f0afbf9b5b1864f78dbc8d7ddc5d599422ce237
Previesam <samsmart.ass59@gmail.com>
2021-02-14 18:40:28 +0100

    Just testing gitignore

--- routes/login.js
+++ routes/login.js
@@ -4,6 +4,7 @@ var router = express.Router();
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('login', { title: 'DreamIt - Login', page_title: 'Login' });
+  console.log('Okay');
 });
 
 module.exports = router;
