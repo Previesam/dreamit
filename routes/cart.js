49220307ae1891422dd14dc82b1f0504fd9fec7d
Previesam <samsmart.ass59@gmail.com>
2021-02-15 23:57:18 +0100

    More update on cart page

--- routes/cart.js
+++ routes/cart.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('cart', { title: 'DreamIt - Cart', page_title: 'Cart' });
+  res.render('cart', { title: 'DreamIt - Cart', page_title: 'Cart', qtyValue: 1 });
   console.log('Okay');
 });
 
